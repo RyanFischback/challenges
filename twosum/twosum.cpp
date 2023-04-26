7bd6b8e2d754c50e2ee4ac5dfd4406af82c1760f
rfischback <ryan-fischback@hotmail.com>
2023-04-26 12:43:40 -0300

    fixed second loop

--- twosum/twosum.cpp
+++ twosum/twosum.cpp
@@ -4,7 +4,7 @@
 //Brute Force O(N^2); O(1);
 std::vector<int> twoSum(std::vector<int>& nums, int target){
     for(int i = 0; i < nums.size(); i++){
-        for(int j = i; j < nums.size() - 1; j++){
+        for(int j = i+1; j < nums.size(); j++){
             if(nums[i] + nums[j] == target){
                 return {i, j};
             }
